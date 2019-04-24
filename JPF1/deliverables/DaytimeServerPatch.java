65b79b7c9f181461d3e6cf5c219c95e047ca2f79
Josefin Nilsson <josefin.l.nilsson@gmail.com>
2019-04-24 15:57:30 +0200

    Add patch for daytime to deliverables

---
+++ JPF1/deliverables/DaytimeServerPatch.java
@@ -0,0 +1,47 @@
+/* $Id: DaytimeServer.java 838 2010-11-24 08:16:23Z cartho $ */
+
+/* Daytime server (implementing RFC 867).
+ * This modified version aborts after having served one client. */
+
+import java.io.IOException;
+import env.java.io.OutputStreamWriter;
+import env.java.net.ServerSocket;
+import env.java.net.Socket;
+import env.java.util.Date;
+
+public class DaytimeServer {
+
+  public final static int PORT = 1024;  //13;
+
+  public static void main(String[]args) {
+    Socket connection = null;
+    ServerSocket server = null;
+    try {
+      server = new ServerSocket(PORT);
+
+      while (true) {
+        connection = server.accept();
+        OutputStreamWriter out
+          = new OutputStreamWriter(connection.getOutputStream());
+        Date now = new Date();
+        out.write(now.toString() + "\n");
+        out.flush();
+      }
+    }
+    catch(IOException e) {
+      System.err.println(e);
+    }
+    finally {
+      try {
+        if(connection!=null){
+          connection.close();
+        }
+	      server.close();
+    }
+      catch(IOException e) {
+        System.err.println(e);
+      }
+      System.out.println("Connection closed.");
+    }
+  }
+}
