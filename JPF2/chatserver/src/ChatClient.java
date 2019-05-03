001c9f23e378e5c89d479d26ec8ae104ab509846
Josefin Nilsson <josefnil@kth.se>
2019-05-03 11:58:40 +0200

    Update client

--- JPF2/chatserver/src/ChatClient.java
+++ JPF2/chatserver/src/ChatClient.java
@@ -1,6 +1,7 @@
 /* $Id: ChatClient.java 723 2009-09-24 07:48:58Z cartho $ */
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -31,18 +32,15 @@ public class ChatClient {
             InputStreamReader istr =
                 new InputStreamReader(socket.getInputStream());
             BufferedReader in = new BufferedReader(istr);
-            PrintWriter out =
-                new PrintWriter(socket.getOutputStream(),true);
-            out.write(id + ": Hello, world!\n");
+            BufferedWriter out =
+                new BufferedWriter(new PrintWriter(socket.getOutputStream()));
+            out.write("Hello, world!\n");
             out.flush();
-            while (in.ready()) {
-              String input = in.readLine();
-              if (input.equals("Server busy") || input.matches("\\[\\d+\\]\\d+: Hello, world!") ||
-              input.matches("Client \\d+ quit.")) {
+            String input;
+            while ((input = in.readLine()) != null) {
+              assert (input.equals("Server busy") || input.matches("\\[\\d+\\]\\d+: (.*)") ||
+              input.matches("Client \\d+ quit."));
                 System.out.println(id + ": Received " + input);
-              } else {
-                assert false;
-              }
             }
             in.close();
             out.close();
