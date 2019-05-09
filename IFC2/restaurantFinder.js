b3986faf392fae501e620d47431e3854ca8b58b8
Josefin Nilsson <josefin.l.nilsson@gmail.com>
2019-05-09 16:54:28 +0200

    Task 3: implement functionality that allows Charlie to guess restaurant

--- IFC2/restaurantFinder.js
+++ IFC2/restaurantFinder.js
@@ -8,14 +8,22 @@ let restaurants = [
 	{ name: 'Restaurant7', locX: -10, locY: -10, avgPrice: 75 },
 	{ name: 'Restaurant8', locX: -5, locY: 25, avgPrice: 105 },
 	{ name: 'Restaurant9', locX: 90, locY: -100, avgPrice: 95 },
-	{ name: 'Restaurant10', locX: 0, locY: 10, avgPrice: 100 },
+	{ name: 'Restaurant10', locX: 12, locY: 10, avgPrice: 100 },
 ]
+
+let xa = lbl(76, 'alice')
+let ya = lbl(-10, 'alice')
+let xb = lbl(222, 'bob')
+let yb = lbl(13, 'bob')
+const avg = 0
+let xc = lbl(100, 'charlie')
+let yc = lbl(100, 'charlie')
 
 function getDist(x1, y1, x2, y2) {
 	return Math.sqrt(Math.pow((x1-x2), 2) + Math.pow((y1-y2), 2))
 }
 
-function nearestRestaurant(xa, ya, xb, yb, avg) {
+function nearestRestaurant() {
 	let closestDistance = lbl(Number.MAX_VALUE, 'alice', 'bob')
 	let closestRestaurant = lbl(restaurants[0], 'alice', 'bob')
 	for (let i = 0; i < restaurants.length; i++) {
@@ -30,11 +38,35 @@ function nearestRestaurant(xa, ya, xb, yb, avg) {
 	return closestRestaurant
 }
 
-const xa = lbl(0, 'alice')
-const ya = lbl(0, 'alice')
-const xb = lbl(1, 'bob')
-const yb = lbl(1, 'bob')
-const price = 100
+function chooseRestaurant() {
+	const rest = nearestRestaurant()
+	xa = rest.locX
+	ya = rest.locY
+	xb = rest.locX
+	yb = rest.locY
+	return rest
+}
 
-lprint(nearestRestaurant(xa, ya, xb, yb, price))
+function distanceAliceCharlie() {
+	return getDist(declassify(xa), declassify(ya), xc, yc)
+}
+
+function guessRestaurant(dist) {
+	let closestDistance = lbl(Number.MAX_VALUE, 'charlie')
+	let closestRestaurant = lbl(restaurants[0], 'charlie')
+	for (let i = 0; i < restaurants.length; i++) {
+		const r = restaurants[i]
+		if (dist === getDist(r.locX, r.locY, xc, yc)) {
+			print(r)
+			closestRestaurant = r
+		}
+	}
+	return closestRestaurant
+}
+
+lprint(chooseRestaurant())
+let dist = distanceAliceCharlie()
+lprint(guessRestaurant(dist))
+
+
 
